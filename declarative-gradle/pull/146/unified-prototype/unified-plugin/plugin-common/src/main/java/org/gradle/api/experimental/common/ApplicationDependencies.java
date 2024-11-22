57c77465288c7262670bd59258650e62225d8099
eskatos <eskatos@users.noreply.github.com>
2024-11-22 09:40:20 +0000

    deploy: gradle/declarative-gradle@4fba0fc4245f408e3c61bba82f5e092b4183cd38

---
+++ declarative-gradle/pull/146/unified-prototype/unified-plugin/plugin-common/src/main/java/org/gradle/api/experimental/common/ApplicationDependencies.java
@@ -0,0 +1,9 @@
+package org.gradle.api.experimental.common;
+
+import org.gradle.declarative.dsl.model.annotations.Restricted;
+
+/**
+ * The declarative dependencies DSL block for an application.
+ */
+@Restricted
+public interface ApplicationDependencies extends BasicDependencies {}
