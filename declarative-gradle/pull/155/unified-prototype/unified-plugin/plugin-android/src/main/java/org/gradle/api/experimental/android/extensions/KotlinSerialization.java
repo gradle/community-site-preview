3e014cd2f61e24eb6598eb5da68a32d2be303f6f
oleg-nenashev <oleg-nenashev@users.noreply.github.com>
2025-01-07 09:39:36 +0000

    deploy: gradle/declarative-gradle@d26df7d6fda8d5a554137ac5bc2f866e96ea32bf

---
+++ declarative-gradle/pull/155/unified-prototype/unified-plugin/plugin-android/src/main/java/org/gradle/api/experimental/android/extensions/KotlinSerialization.java
@@ -0,0 +1,43 @@
+package org.gradle.api.experimental.android.extensions;
+
+import org.gradle.api.Action;
+import org.gradle.api.artifacts.dsl.Dependencies;
+import org.gradle.api.artifacts.dsl.DependencyCollector;
+import org.gradle.api.provider.Property;
+import org.gradle.api.tasks.Nested;
+import org.gradle.declarative.dsl.model.annotations.Configuring;
+import org.gradle.declarative.dsl.model.annotations.Restricted;
+
+/**
+ * Extension to manage setting up Kotlin serialization for a Kotlin Android library.
+ */
+@SuppressWarnings("UnstableApiUsage")
+@Restricted
+public interface KotlinSerialization {
+    @Restricted
+    Property<Boolean> getEnabled();
+
+    /**
+     * Kotlin serialization library version to use.
+     */
+    @Restricted
+    Property<String> getVersion();
+
+    @Nested
+    SerializationDependencies getDependencies();
+
+    @Configuring
+    default void dependencies(Action<? super SerializationDependencies> action) {
+        action.execute(getDependencies());
+    }
+
+    @Restricted
+    Property<Boolean> getJsonEnabled();
+
+    @Restricted
+    interface SerializationDependencies extends Dependencies {
+        DependencyCollector getImplementation();
+    }
+}
+
+
