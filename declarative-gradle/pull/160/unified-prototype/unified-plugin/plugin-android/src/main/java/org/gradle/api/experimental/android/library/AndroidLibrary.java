c9929a61d7c8e974b7fb3eea191c37200dc92d88
eskatos <eskatos@users.noreply.github.com>
2025-01-24 10:19:31 +0000

    deploy: gradle/declarative-gradle@1a47625e28f5ff420efbdf4ac09e4c0b47eb924b

---
+++ declarative-gradle/pull/160/unified-prototype/unified-plugin/plugin-android/src/main/java/org/gradle/api/experimental/android/library/AndroidLibrary.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gradle.api.experimental.android.library;
+
+import org.gradle.api.Action;
+import org.gradle.api.experimental.android.AndroidSoftware;
+import org.gradle.api.provider.Property;
+import org.gradle.api.experimental.android.extensions.Protobuf;
+import org.gradle.api.tasks.Nested;
+import org.gradle.declarative.dsl.model.annotations.Configuring;
+import org.gradle.declarative.dsl.model.annotations.Restricted;
+
+@Restricted
+public interface AndroidLibrary extends AndroidSoftware {
+    @Override
+    @Nested
+    AndroidLibraryDependencies getDependencies();
+
+    @Configuring
+    default void dependencies(Action<? super AndroidLibraryDependencies> action) {
+        action.execute(getDependencies());
+    }
+
+    @Override
+    @Nested
+    AndroidLibraryBuildTypes getBuildTypes();
+
+    @Configuring
+    default void buildTypes(Action<? super AndroidLibraryBuildTypes> action) {
+        action.execute(getBuildTypes());
+    }
+
+    // TODO:DG We really want to model a list of consumer proguard files here, but can't yet
+    @Restricted
+    Property<String> getConsumerProguardFile();
+
+    @Nested
+    Protobuf getProtobuf();
+
+    @Configuring
+    default void protobuf(Action<? super Protobuf> action) {
+        action.execute(getProtobuf());
+    }
+
+    /**
+     * Flag to enable/disable generation of the `BuildConfig` class.
+     *
+     * Default value is `false`.
+     */
+    @Restricted
+    Property<Boolean> getBuildConfig();
+}
