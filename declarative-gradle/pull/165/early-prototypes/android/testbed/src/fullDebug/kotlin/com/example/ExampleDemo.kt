8838fa6213e45c42f953b41fec6f06e7aee03d36
blindpirate <blindpirate@users.noreply.github.com>
2025-02-18 09:22:01 +0000

    deploy: gradle/declarative-gradle@df408bc9fee64a4aea274f39978a38e1ac585df9

--- declarative-gradle/pull/165/early-prototypes/android/testbed/src/fullDebug/kotlin/com/example/ExampleDemo.kt
+++
@@ -1,9 +0,0 @@
-package com.example
-
-// This passes since we declared an okio dependency on the fullDebug target
-
-class ExampleDemo(example: Example) {
-    init {
-        okio.Buffer()
-    }
-}
