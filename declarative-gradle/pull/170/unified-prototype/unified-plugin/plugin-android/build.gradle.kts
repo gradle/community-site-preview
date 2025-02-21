bcfb792c1a040835e2440ff1a9b6457be548d7be
tresat <tresat@users.noreply.github.com>
2025-02-21 21:21:43 +0000

    deploy: gradle/declarative-gradle@b221b3282373161b773f7d33cb567189d8656b80

---
+++ declarative-gradle/pull/170/unified-prototype/unified-plugin/plugin-android/build.gradle.kts
@@ -0,0 +1,94 @@
+@file:Suppress("UnstableApiUsage")
+
+plugins {
+    `kotlin-dsl`
+    id("build-logic.publishing")
+    groovy // For spock testing
+}
+
+description = "Implements the declarative Android DSL prototype"
+
+dependencies {
+    api(project(":plugin-common"))
+    api(libs.android.agp.application)
+    api(libs.android.kotlin.android)
+
+    implementation(project(":plugin-jvm"))
+    implementation(libs.baseline.profile.plugin)
+    implementation(libs.dependency.guard.plugin)
+    implementation(libs.ksp.plugin)
+    implementation(libs.hilt.android.plugin)
+    implementation(libs.kotlin.serialization.plugin)
+    implementation(libs.room.plugin)
+    implementation(libs.protobuf.plugin)
+    implementation(libs.roborazzi.plugin)
+    implementation(libs.google.services.plugin)
+    implementation(libs.firebase.perf.plugin)
+    implementation(libs.firebase.crashlytics.plugin)
+    implementation(libs.oss.licenses.plugin)
+    implementation(libs.compose.compiler.plugin)
+    implementation(libs.secrets.plugin)
+
+    implementation(libs.apache.commons.lang)
+    implementation(libs.android.tools.common)
+    implementation(libs.truth)
+}
+
+testing {
+    suites {
+        @Suppress("UnstableApiUsage")
+        val integTest by registering(JvmTestSuite::class) {
+            useSpock("2.2-groovy-3.0")
+
+            dependencies {
+                implementation(project(":internal-testing-utils"))
+            }
+        }
+
+        tasks.getByPath("check").dependsOn(integTest)
+    }
+}
+
+gradlePlugin {
+    testSourceSets(project.sourceSets.getByName("integTest"))
+
+    plugins {
+        create("android-library") {
+            id = "org.gradle.experimental.android-library"
+            displayName = "Android Library Experimental Declarative Plugin"
+            description = "Experimental declarative plugin for Android libraries"
+            implementationClass = "org.gradle.api.experimental.android.library.StandaloneAndroidLibraryPlugin"
+            tags = setOf("declarative-gradle", "android")
+        }
+        create("android-application") {
+            id = "org.gradle.experimental.android-application"
+            displayName = "Android Application Experimental Declarative Plugin"
+            description = "Experimental declarative plugin for Android applications"
+            implementationClass = "org.gradle.api.experimental.android.application.StandaloneAndroidApplicationPlugin"
+            tags = setOf("declarative-gradle", "android")
+        }
+        create("android-test") {
+            id = "org.gradle.experimental.android-test"
+            displayName = "Android Test Experimental Declarative Plugin"
+            description = "Experimental declarative plugin for Android test projects"
+            implementationClass = "org.gradle.api.experimental.android.test.StandaloneAndroidTestPlugin"
+            tags = setOf("declarative-gradle", "android")
+        }
+        create("android-ecosystem") {
+            id = "org.gradle.experimental.android-ecosystem"
+            displayName = "Android Ecosystem Experimental Declarative Plugin"
+            description = "Experimental declarative plugin for the Android ecosystem"
+            implementationClass = "org.gradle.api.experimental.android.AndroidEcosystemPlugin"
+            tags = setOf("declarative-gradle", "android")
+        }
+    }
+}
+
+// Compile against Java 17 since Android requires Java 17 at minimum
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
---
+++ declarative-gradle/pull/170/unified-prototype/unified-plugin/plugin-android/src/main/java/org/gradle/api/experimental/android/extensions/CoreLibraryDesugaring.java
@@ -0,0 +1,16 @@
+package org.gradle.api.experimental.android.extensions;
+
+import org.gradle.api.provider.Property;
+import org.gradle.declarative.dsl.model.annotations.Restricted;
+
+@Restricted
+public interface CoreLibraryDesugaring {
+    @Restricted
+    Property<Boolean> getEnabled();
+
+    /**
+     * The version of the library to use for desugaring.
+     */
+    @Restricted
+    Property<String> getLibVersion();
+}
