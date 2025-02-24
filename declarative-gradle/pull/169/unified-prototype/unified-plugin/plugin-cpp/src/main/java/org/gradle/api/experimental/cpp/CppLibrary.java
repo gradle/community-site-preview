3d11becf9bfa1b2531705108a1e4c601622e1b0e
jbartok <jbartok@users.noreply.github.com>
2025-02-24 07:33:42 +0000

    deploy: gradle/declarative-gradle@6f46a85bf9d0c0016a6db642c1f6b96d5cc8932c

--- declarative-gradle/pull/169/unified-prototype/unified-plugin/plugin-cpp/src/main/java/org/gradle/api/experimental/cpp/CppLibrary.java
+++
@@ -1,6 +0,0 @@
-package org.gradle.api.experimental.cpp;
-
-import org.gradle.api.experimental.common.HasLibraryDependencies;
-
-public interface CppLibrary extends HasCppTarget, HasLibraryDependencies {
-}
