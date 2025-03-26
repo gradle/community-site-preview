74edb5e6461ed9958dd327350ffc9f44bf65527e
big-guy <big-guy@users.noreply.github.com>
2025-03-26 15:53:53 +0000

    deploy: gradle/declarative-gradle@dad0847a4de090d9109d684b9a800688d6bf00c6

--- declarative-gradle/pull/184/unified-prototype/unified-plugin/plugin-jvm/src/main/java/org/gradle/api/experimental/jvm/JvmLibrary.java
+++
@@ -1,9 +0,0 @@
-package org.gradle.api.experimental.jvm;
-
-import org.gradle.api.experimental.common.HasLibraryDependencies;
-
-/**
- * A library that runs on the JVM and that is implemented using one or more versions of Java.
- */
-public interface JvmLibrary extends HasJavaTargets, HasLibraryDependencies {
-}
