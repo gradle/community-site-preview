ce8812694e935abe99b5e4fcfc9396a21a147a37
tresat <tresat@users.noreply.github.com>
2025-04-09 13:47:24 +0000

    deploy: gradle/declarative-gradle@e7d9d9634eab48cd1578c335b929b7ad3b067cb2

--- declarative-gradle/pull/178/unified-prototype/unified-plugin/plugin-swift/src/main/java/org/gradle/api/experimental/swift/SwiftLibrary.java
+++
@@ -1,6 +0,0 @@
-package org.gradle.api.experimental.swift;
-
-import org.gradle.api.experimental.common.HasLibraryDependencies;
-
-public interface SwiftLibrary extends HasSwiftTarget, HasLibraryDependencies {
-}
