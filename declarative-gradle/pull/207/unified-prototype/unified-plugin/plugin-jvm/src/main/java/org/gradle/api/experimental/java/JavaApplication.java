12125696260091776cb988f743fccf1a1ce1f090
ghale <ghale@users.noreply.github.com>
2025-11-19 15:11:26 +0000

    deploy: gradle/declarative-gradle@4a0589818d53cec11786da75fe5ac9cc13689f5d

---
+++ declarative-gradle/pull/207/unified-prototype/unified-plugin/plugin-jvm/src/main/java/org/gradle/api/experimental/java/JavaApplication.java
@@ -0,0 +1,25 @@
+package org.gradle.api.experimental.java;
+
+import org.gradle.api.Action;
+import org.gradle.api.experimental.common.HasCliExecutables;
+import org.gradle.api.experimental.jvm.HasJavaTarget;
+import org.gradle.api.experimental.jvm.HasJvmApplication;
+import org.gradle.api.experimental.jvm.JavaApplicationBuildModel;
+import org.gradle.api.experimental.jvm.extensions.testing.Testing;
+import org.gradle.api.internal.plugins.Definition;
+import org.gradle.api.tasks.Nested;
+import org.gradle.declarative.dsl.model.annotations.Configuring;
+import org.jspecify.annotations.NonNull;
+
+/**
+ * An application implemented using a single version of Java.
+ */
+public interface JavaApplication extends HasJavaTarget, HasJvmApplication, Definition<@NonNull JavaApplicationBuildModel> {
+    @Nested
+    Testing getTesting();
+
+    @Configuring
+    default void testing(Action<? super Testing> action) {
+        action.execute(getTesting());
+    }
+}
