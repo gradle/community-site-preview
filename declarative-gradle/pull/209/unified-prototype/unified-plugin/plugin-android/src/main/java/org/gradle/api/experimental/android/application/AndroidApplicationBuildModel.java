33ebb0d98ea0f9f9ea074d8c79de01de85bb2e42
ghale <ghale@users.noreply.github.com>
2025-12-03 12:29:51 +0000

    deploy: gradle/declarative-gradle@d259e10580ccd1f6bb1cfb57f9bae5733be6ef51

--- declarative-gradle/pull/209/unified-prototype/unified-plugin/plugin-android/src/main/java/org/gradle/api/experimental/android/application/AndroidApplicationBuildModel.java
+++
@@ -1,8 +0,0 @@
-package org.gradle.api.experimental.android.application;
-
-import com.android.build.api.dsl.ApplicationExtension;
-import org.gradle.api.internal.plugins.BuildModel;
-
-public interface AndroidApplicationBuildModel extends BuildModel {
-    ApplicationExtension getApplicationExtension();
-}
--- declarative-gradle/pull/209/unified-prototype/unified-plugin/plugin-android/src/main/java/org/gradle/api/experimental/android/test/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package models the result of applying the "com.android.test" plugin.
- */
-@NullMarked
-package org.gradle.api.experimental.android.test;
-
-import org.jspecify.annotations.NullMarked;
