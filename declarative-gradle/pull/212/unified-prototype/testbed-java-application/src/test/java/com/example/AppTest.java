35137e178787cd4d46bfd00384664ae5890e052f
cobexer <cobexer@users.noreply.github.com>
2025-12-09 19:06:10 +0000

    deploy: gradle/declarative-gradle@f3874cd17d1b15251ab2b4f374ef8cb0a2230923

---
+++ declarative-gradle/pull/212/unified-prototype/testbed-java-application/src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    void appHasTest() {
+        assert true;
+    }
+}
---
+++ declarative-gradle/pull/212/unified-prototype/unified-plugin/plugin-kmp/src/main/java/org/gradle/api/experimental/kmp/KmpLibrary.java
@@ -0,0 +1,33 @@
+package org.gradle.api.experimental.kmp;
+
+import org.gradle.api.Action;
+import org.gradle.api.experimental.common.HasGroupAndVersion;
+import org.gradle.api.experimental.common.HasLibraryDependencies;
+import org.gradle.api.internal.plugins.Definition;
+import org.gradle.api.provider.Property;
+import org.gradle.api.tasks.Input;
+import org.gradle.api.tasks.Internal;
+import org.gradle.api.tasks.Nested;
+import org.gradle.declarative.dsl.model.annotations.Configuring;
+
+/**
+ * The public DSL interface for a declarative KMP library.
+ */
+public interface KmpLibrary extends HasLibraryDependencies, HasGroupAndVersion, Definition<KotlinMultiplatformBuildModel> {
+    @Input
+    Property<String> getLanguageVersion();
+
+    @Internal
+    @Nested
+    KmpLibraryTargetContainer getTargetsContainer();
+
+    @Nested
+    default StaticKmpLibraryTargets getTargets() {
+        return getTargetsContainer();
+    }
+
+    @Configuring
+    default void targets(Action<? super StaticKmpLibraryTargets> action) {
+        action.execute(getTargets());
+    }
+}
