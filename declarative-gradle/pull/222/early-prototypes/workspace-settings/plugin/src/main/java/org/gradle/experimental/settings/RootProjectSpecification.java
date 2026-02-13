b1edc6456cbe82b65afb6a0c910f620256039c17
ghale <ghale@users.noreply.github.com>
2026-02-13 12:42:26 +0000

    deploy: gradle/declarative-gradle@e8b58b355a4b3ea4a54898944b3a4a6b243437e9

--- declarative-gradle/pull/222/early-prototypes/workspace-settings/plugin/src/main/java/org/gradle/experimental/settings/RootProjectSpecification.java
+++
@@ -1,7 +0,0 @@
-package org.gradle.experimental.settings;
-
-import org.gradle.api.provider.Property;
-
-public interface RootProjectSpecification extends ProjectContainer {
-    Property<Boolean> getAutodetect();
-}
