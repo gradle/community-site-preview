f81c6e971a768c1b7d44f6350e9fa1658d932584
ghale <ghale@users.noreply.github.com>
2026-03-19 00:30:29 +0000

    deploy: gradle/declarative-gradle@03d3c29092121c07e341ea9a44e8f5f8c6081ecb

--- declarative-gradle/pull/229/unified-prototype/unified-plugin/plugin-android/src/main/java/org/gradle/api/experimental/android/test/AndroidTestBuildModel.java
+++
@@ -1,9 +0,0 @@
-package org.gradle.api.experimental.android.test;
-
-import com.android.build.api.dsl.TestExtension;
-import org.gradle.features.binding.BuildModel;
-
-@SuppressWarnings("UnstableApiUsage")
-public interface AndroidTestBuildModel extends BuildModel {
-    public TestExtension getTestExtension();
-}
--- declarative-gradle/pull/229/unified-prototype/unified-plugin/plugin-swift/src/main/java/org/gradle/api/experimental/swift/SwiftLibrary.java
+++
@@ -1,8 +0,0 @@
-package org.gradle.api.experimental.swift;
-
-import org.gradle.api.experimental.common.HasLibraryDependencies;
-import org.gradle.features.binding.Definition;
-
-@SuppressWarnings("UnstableApiUsage")
-public interface SwiftLibrary extends HasSwiftTarget, HasLibraryDependencies, Definition<SwiftLibraryBuildModel> {
-}
